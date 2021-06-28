b2d5f1eaed25122f5e412a3f9d437e293dd71e5e
rcourivaud <r.courivaiud@gmail.com>
2021-06-28 16:02:45 +0200

    fix daily index creation
    Signed-off-by: rcourivaud <r.courivaiud@gmail.com>

--- amqp/event-logger/app/parser.py
+++ amqp/event-logger/app/parser.py
@@ -1,5 +1,10 @@
 import xmltodict
 import json 
+import xml.dom.minidom
+
+
+def get_xml_from_tag_name(xml, tag):
+    return xml.getElementsByTagName(tag)
 
 def parse_xml_file_to_dict(filename):
     with open(filename) as xml_file:
@@ -12,8 +17,9 @@ def parse_xml_string_to_dict(xml_string):
     return json.loads(json.dumps(data_dict))
 
 
-def get_protocol_from_xml(xml):
-    message =  get_xml_from_tag_name(xml=xml, tag="message")[0]
+def get_protocol_from_xml_string(xml_string):
+    xml_ = xml.dom.minidom.parseString(xml_string)
+    message =  get_xml_from_tag_name(xml=xml_, tag="message")[0]
     protocol = message.getAttribute("xmlns").split(":")[-2:-1][0]
     return protocol
 
--- amqp/event-logger/app/runner.py
+++ amqp/event-logger/app/runner.py
@@ -3,7 +3,7 @@ import sys
 from aio_pika import connect, IncomingMessage, ExchangeType, Message, DeliveryMode, Exchange, Channel
 import os 
 from functools import partial
-from parser import parse_xml_string_to_dict, get_protocol_from_xml
+from parser import parse_xml_string_to_dict, get_protocol_from_xml_string
 from elasticsearch import Elasticsearch
 from datetime import datetime
 
@@ -13,10 +13,11 @@ ELASTIC_URI = os.environ.get("ELASTIC_URI",  "0.0.0.0:9200")
 
 async def on_message_log_it(es_client: Elasticsearch, message: IncomingMessage):
     xml_string = message.body.decode()
-    protocol = get_protocol_from_xml(xml_string)
+    protocol = get_protocol_from_xml_string(xml_string)
     edxl_data = parse_xml_string_to_dict(message.body.decode())
+    print(f"logging on routing-{protocol}-{datetime.today().strftime('%Y-%m-%d')}")
     res = es_client.index(
-        index=f"routing-{xml_string}-{datetime.today().strftime('%Y-%m-%d')}", 
+        index=f"routing-{protocol}-{datetime.today().strftime('%Y-%m-%d')}", 
         id=edxl_data["edxlDistribution"]["distributionID"], 
         body=edxl_data
     )
