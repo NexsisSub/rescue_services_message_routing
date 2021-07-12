95759c4091e63d10f22f38778b57b8ea42e3f299
rcourivaud <r.courivaiud@gmail.com>
2021-07-12 10:13:00 +0200

    fix uuid generation in pydantic event schema
    Signed-off-by: rcourivaud <r.courivaiud@gmail.com>

--- amqp/dispatcher/app/schema.py
+++ amqp/dispatcher/app/schema.py
@@ -1,10 +1,11 @@
 from typing import List, Optional
 from datetime import datetime
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from uuid import uuid4
+from typing_extensions import Annotated
 
 class Event(BaseModel):
-    id: str = str(uuid4())
+    id: Annotated[str, Field(default_factory=lambda: uuid4().hex)]
     created_at: datetime = datetime.now()
     routed_at: datetime
     raw: str 
